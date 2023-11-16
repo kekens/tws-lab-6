e7cd0b24a1e28e0194ecd7d943ae0d08f81bfc21
Kirill Bobryakov <kirillbobryakov82@gmail.com>
2023-11-17 01:06:31 +0300

    test commit

--- tws-4-client/src/main/java/ru/kekens/RestfulServiceClient.java
+++ tws-4-client/src/main/java/ru/kekens/RestfulServiceClient.java
@@ -1,10 +1,7 @@
 package ru.kekens;
 
-import java.net.MalformedURLException;
-import java.text.ParseException;
-
 public class RestfulServiceClient {
-    public static void main(String[] args) throws MalformedURLException, ParseException {
+    public static void main(String[] args) {
     }
 
 }
